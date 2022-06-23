7378a67320210060040447fb64740c84c16c24a1
Sewer 56 <seweratwork@gmail.com>
2022-06-23 15:01:19 +0100

    Added: Default Values to Mod Configuration

--- Source/Config.cs
+++ Source/Config.cs
@@ -7,9 +7,11 @@ public class Config : Configurable<Config>
 {
     [DisplayName("Minimum Draw Distance")]
     [Description("Minimum object draw distance, between 0 and 255.")]
+    [DefaultValue((byte)0)]
     public byte MinimumDrawDistance { get; set; } = 0;
 
     [DisplayName("Draw Distance Multiplier")]
     [Description("Draw distance of this object is scaled by this value.")]
+    [DefaultValue((float)2.0)]
     public float DrawDistanceMultiplier { get; set; } = 2;
 }
